57fbcea23df1feebfb9d4966ad9357e1471d476b
axavier <axavier@cmcorp.com.br>
2017-07-24 22:22:20 -0300

    criação do formulario para contato usando o plugin contact form 7

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -27,8 +27,25 @@
     <div class="container">
         <div class="row">
             <article>
-                <div class="icons col-xs-6 col-lg-4 col-sm-6">
-                   <form role="form">
+                
+                <div class="col-lg-3 col-sm-6">              
+                    <a href="https://www.linkedin.com/in/anamarcelaxs" title="Linkedin" target="_blank"><span class="linkedin"><span></a>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <a href="skype:amarcelaxs" title="Linkedin" target="_blank"><span class="skype"><span></a>
+                </div>  
+                <div class="col-lg-3 col-sm-6">  
+                      <a href="https://www.github.com/amarcelaxs" title="Linkedin" target="_blank"><span class="cat"><span></a>
+                </div>      
+                <div class="col-lg-3 col-sm-6">
+                    <a href="Curriculo-AnaMarcela.pdf" title="Curriculo" target="_blank"><span class="pdf"></span></a>              
+                </div>                
+            </article>
+        </div>
+        <div class="row">
+            <div class="  col-xs-12 col-lg-8 col-sm-12 col-lg-offset-2">
+               
+                   <!--form role="form">
                         <div class="form-group">
                             <label>Nome:</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o seu nome">
@@ -45,22 +62,11 @@
                             <button type="submit" class="btn btn-default">Cadastrar</button>
                             <a href="#" target="_blank">Remova meu cadatro</a>
                         </div>
-                    </form>              
+                    </form-->            
+                    <?php echo do_shortcode( '[contact-form-7 id="8" title="Contato"]' ); ?>  
                 </div>
-                <div class="col-lg-2 col-sm-6">              
-                    <a href="https://www.linkedin.com/in/anamarcelaxs" title="Linkedin" target="_blank"><span class="linkedin"><span></a>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <a href="skype:amarcelaxs" title="Linkedin" target="_blank"><span class="skype"><span></a>
-                </div>  
-                <div class="col-lg-2 col-sm-6">  
-                      <a href="https://www.github.com/amarcelaxs" title="Linkedin" target="_blank"><span class="cat"><span></a>
-                </div>      
-                <div class="col-lg-2 col-sm-6">
-                    <a href="Curriculo-AnaMarcela.pdf" title="Curriculo" target="_blank"><span class="pdf"></span></a>              
-                </div>
-            </article>
         </div>
+        
     </div>
        
 </section>
